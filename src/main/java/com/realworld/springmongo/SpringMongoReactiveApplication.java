a09cc960e47bb10ed971f59ebd43fbaf7a3cc8c5
a-mountain <frostnext2@gmail.com>
2021-07-17 23:07:40 +0300

    Initial commit

---
+++ src/main/java/com/realworld/springmongo/SpringMongoReactiveApplication.java
@@ -0,0 +1,11 @@
+package com.realworld.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoReactiveApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoReactiveApplication.class, args);
+	}
+}
